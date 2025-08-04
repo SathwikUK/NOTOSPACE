9650b56b610db5dc15fbb3cc5b19ffb017ee2399
SathwikUK <sathwiksampengala@gmail.com>
2025-08-04 22:25:40 +0530

    vercel file

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "https://notospace.vercel.app"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://notospace.vercel.app/"],
   credentials: true
 }));
 
