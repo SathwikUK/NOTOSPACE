6e2654bb1b442dc016cbcece4aab6901ccb8dd4d
SathwikUK <sathwiksampengala@gmail.com>
2025-08-04 21:19:24 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "*",
+  origin: "https://notospacebackend.vercel.app",
   credentials: true
 }));
 app.use(express.json());
